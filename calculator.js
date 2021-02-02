304e3d8aec183f8d9fa9f71443255d4b24b626ed
AishahS <aishahalsaqan@gmail.com>
2021-02-02 19:23:32 +0300

    Finally is donw

--- calculator.js
+++ calculator.js
@@ -1,3 +1,12 @@
 // Define the numbers and the operation here
+let a = 5;
+let b = 3;
+let addingtwoNumbers = true ;
 
 // Write if-statements here
+
+if (addingtwoNumbes = true) {
+  
+    console.log(`${a} + ${b} = ${a + b}`)
+    console.log(`The result is  ${a + b}`)
+  };
