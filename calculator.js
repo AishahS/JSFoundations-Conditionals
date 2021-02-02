b12e2903c440abe2daad914020ddebce78df77ce
AishahS <aishahalsaqan@gmail.com>
2021-02-02 20:10:33 +0300

    Solved the Conditionals task

--- calculator.js
+++ calculator.js
@@ -1,12 +1,18 @@
 // Define the numbers and the operation here
-let a = 5;
-let b = 3;
-let addingtwoNumbers = true ;
+let a = 12;
+let b = 6;
+let oproftwoNumbers = "adding";
 
 // Write if-statements here
 
-if (addingtwoNumbes = true) {
+if (oproftwoNumbes = false) {
   
     console.log(`${a} + ${b} = ${a + b}`)
     console.log(`The result is  ${a + b}`)
+  } else
+
+if  (oproftwoNumbes = true) {
+      console.log(`${a} / ${b} = ${a / b}`)
+      console.log(`The result is  ${a / b}`)
+    
   };
