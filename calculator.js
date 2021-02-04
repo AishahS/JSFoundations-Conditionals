c58267846451751caa3b6f593afff0f8218196b9
AishahS <aishahalsaqan@gmail.com>
2021-02-04 09:43:24 +0300

    done and its work

--- calculator.js
+++ calculator.js
@@ -1,18 +1,20 @@
-// Define the numbers and the operation here
-let a = 12;
-let b = 6;
-let oproftwoNumbers = "adding";
+var a = 12;
+var b = 6;
+var additional = "additional"; // setting a value to additional variable..
+var oproftwoNumbers = additional;
 
 // Write if-statements here
 
-if (oproftwoNumbes = false) {
-  
-    console.log(`${a} + ${b} = ${a + b}`)
-    console.log(`The result is  ${a + b}`)
-  } else
-
-if  (oproftwoNumbes = true) {
-      console.log(`${a} / ${b} = ${a / b}`)
-      console.log(`The result is  ${a / b}`)
-    
-  };
+if (oproftwoNumbers === additional) {
+  console.log(`${a} + ${b} = ${a + b}`);
+  console.log(`The result is  ${a + b}`);
+} else if (oproftwoNumbers === divide) {
+  console.log(`${a} / ${b} = ${a / b}`);
+  console.log(`The result is  ${a / b}`);
+} else if (oproftwoNumbers === subtract) {
+  console.log(`${a} - ${b} = ${a - b}`);
+  console.log(`The result is  ${a - b}`);
+} else if (oproftwoNumbers === multiply) {
+  console.log(`${a} * ${b} = ${a * b}`);
+  console.log(`The result is  ${a * b}`);
+}
